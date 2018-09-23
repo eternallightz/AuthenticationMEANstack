ea57d165caee75890293b0a4d7a85788119596f2
eternallightz <eternallightz107@gmail.com>
2018-09-24 02:33:49 +0300

    Connect to MongoDB using Mongoose

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,15 @@
  const express = require('express');
  const router = express.Router();
+ const mongoose = require('mongoose');
+
+ const db = 'mongodb://usernika:nikita1@ds213053.mlab.com:13053/eventsdb';
+ mongoose.connect(db, err => {
+     if (err){
+        console.error('Error!' + err);
+     } else {
+        console.log('Connected to mongodb');
+     }
+ });
 
  router.get('/', (rec, res) => (res.send('From API router')));
  module.exports = router;
