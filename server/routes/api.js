54c4c9fdbefa05cd62ff13b2e07a63fd5197dcf7
eternallightz <eternallightz107@gmail.com>
2018-09-24 03:32:31 +0300

    Add Register and Login api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,6 @@
  const express = require('express');
  const router = express.Router();
+ const User = require('../models/user.js');
  const mongoose = require('mongoose');
 
  const db = 'mongodb://usernika:nikita1@ds213053.mlab.com:13053/eventsdb';
@@ -12,4 +13,35 @@
  });
 
  router.get('/', (rec, res) => (res.send('From API router')));
+
+ router.post('/register', (req, res) =>{
+    let userData = req.body;
+    let user = new User(userData);
+    user.save((error, registeredUser) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.status(200).send(registeredUser);
+        }
+    });
+ });
+
+ router.post('/login', (req, res) => {
+    let userData = req.body;
+    User.findOne({email: userData.email}, (error, user) => {
+        if (error) {
+            console.log(error);
+        } else {
+            if (!user) {
+                res.status(401).send('Invalid email');
+            } else
+            if (user.password !== userData.password) {
+                res.status(401).send('Invalid password');
+            } else {
+                res.status(200).send(user);
+            }
+        }
+    });
+ });
+
  module.exports = router;
